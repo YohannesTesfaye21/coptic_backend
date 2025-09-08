961e187d705b7b7a07aabb4a32b32d51ffb916f1
m2pro <yohannestesfaye380@gmail.com>
2025-09-08 18:32:59 +0300

    fix reply and delete chat migration

---
+++ coptic_app_backend.Api/Migrations/20250908152947_AddEditFieldsToChatMessage.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace coptic_app_backend.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEditFieldsToChatMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "EditedAt",
+                table: "ChatMessages",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EditedBy",
+                table: "ChatMessages",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEdited",
+                table: "ChatMessages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EditedAt",
+                table: "ChatMessages");
+
+            migrationBuilder.DropColumn(
+                name: "EditedBy",
+                table: "ChatMessages");
+
+            migrationBuilder.DropColumn(
+                name: "IsEdited",
+                table: "ChatMessages");
+        }
+    }
+}
